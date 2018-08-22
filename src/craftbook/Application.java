c6d3ecba12e651a97b7757a1d44d5d0b179f0cee
Nick White <nawhi3@outlook.com>
2018-08-22 20:08:28 +0100

    Improve package name and add JUnit

--- src/craftbook/Application.java
+++ src/craftbook/Application.java
@@ -1,4 +1,4 @@
-package CraftBook;
+package craftbook;
 
 public class Application {
 
---
+++ src/craftbook/ApplicationTest.java
@@ -0,0 +1,12 @@
+package craftbook;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test(expected=AssertionError.class)
+	public void sanityCheck() {
+		assert false;
+	}
+
+}
