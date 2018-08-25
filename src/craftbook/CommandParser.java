b94b6b9dd24d5ff8dc4b80204546d80205db3094
Nick White <nawhi3@outlook.com>
2018-08-25 15:42:28 +0100

    Implement Command.parse()

--- src/craftbook/CommandParser.java
+++ src/craftbook/CommandParser.java
@@ -1,6 +1,8 @@
 package craftbook;
 
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
 
 class TokenList {
 	public String username;
@@ -43,6 +45,27 @@ public final class CommandParser {
 	 * @throws ParseException if the input cannot be tokenised
 	 */
 	public static TokenList parse(String line) throws ParseException {
-		throw new RuntimeException("TODO");
+		List<String> tokens = Arrays.asList(line.trim().split("\\s+"));
+		
+		if (tokens.size() == 0)
+			throw new ParseException("No tokens found", 0);
+		
+		if (tokens.get(0).isEmpty())
+			throw new ParseException("No username found", 0);
+		
+		String username, command, parameter;
+		username = tokens.get(0);
+		
+		if (tokens.size() > 1)
+			command = tokens.get(1);
+		else
+			command = "";
+		
+		if (tokens.size() > 2)
+			parameter = String.join(" ", tokens.subList(2, tokens.size()));
+		else
+			parameter = "";
+		
+		return new TokenList(username, command, parameter);
 	}
 }
