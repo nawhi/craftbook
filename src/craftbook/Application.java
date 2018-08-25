cdfb9310da9b93d912dbf11182266d701bfcb98c
Nick White <nawhi3@outlook.com>
2018-08-25 16:17:46 +0100

    Implement Application with program entry point and event loop

--- src/craftbook/Application.java
+++ src/craftbook/Application.java
@@ -1,9 +1,58 @@
 package craftbook;
 
+import java.io.Console;
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("hello world!");
+	private Model model = new Model();
+	private CommandFactory commandFactory = new CommandFactory(model);
+	
+	private final String PROMPT = ">";
+	
+	public void runEventLoop() {
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			for (;;) {
+				System.out.print(PROMPT);
+				
+				String input = scanner.nextLine();
+				if (input.equals("!quit")) {
+					System.out.println("Bye");
+					break;
+				}
+				
+				TokenList tokens;
+				try {
+					tokens = CommandParser.parse(input);
+				} catch (ParseException ex) {
+					System.out.println("Error parsing input: " + ex.getMessage());
+					continue;
+				}
+				
+				Command command = commandFactory.makeCommand(tokens);
+				command.execute();
+			}
+		} catch (Exception ex) {
+			System.err.print("Fatal exception");
+			String msg = ex.getMessage();
+			if (msg != null)
+				System.err.print(": " + msg);
+			System.err.println();
+		} finally {
+			scanner.close();
+		}
 	}
 
+	public static void main(String[] args) {
+		
+		// TODO: Handle SIGINT
+		
+		Application app = new Application();
+		app.runEventLoop();
+	}
+	
+	
 }
