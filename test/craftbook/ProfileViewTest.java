02983cbd40028b87120be8ac23b57cd40ddfd43e
Nick White <nawhi3@outlook.com>
2018-08-26 09:11:10 +0100

    Move DummyUser into its own source file, so WallViewTest can use it too

--- test/craftbook/ProfileViewTest.java
+++ test/craftbook/ProfileViewTest.java
@@ -7,34 +7,6 @@ import java.time.Instant;
 import org.junit.Test;
 
 /**
- * Minimal User mock which allows control over the
- * timestamp of posts.
- * Tests must be careful not to supply timestamps that
- * are not in chronological order to sequential posts, 
- * as this would break the postcondition of getPosts().
- * 
- * @author nick
- *
- */
-class DummyUser extends User {
-	
-	public DummyUser(String handle) {
-		super(handle);
-	}
-	
-	/**
-	 * Add a new post with timestamp control.
-	 * NB this is an overload not override, 
-	 * void post(String) is still available!
-	 * @param message the text content of the post 
-	 * @param timestamp time the post was created
-	 */
-	public void post(String message, Instant timestamp) {
-		posts.add(new Post(message, timestamp, this));
-	}
-}
-
-/**
  * Minimal ProfileView mock which allows control over the
  * view's current time.
  * @author nick
