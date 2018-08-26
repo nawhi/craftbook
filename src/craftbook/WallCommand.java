f33b37c38a4041148a693e2df5111507b074163e
Nick White <nawhi3@outlook.com>
2018-08-26 10:05:51 +0100

    Add WallCommand

---
+++ src/craftbook/WallCommand.java
@@ -0,0 +1,15 @@
+package craftbook;
+
+public class WallCommand extends Command {
+
+	public WallCommand(User u) {
+		super(u);
+	}
+
+	@Override
+	public void execute() {
+		WallView view = new WallView(targetUser);
+		view.show();
+	}
+
+}
