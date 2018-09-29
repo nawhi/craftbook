52a6359494bcfc180ec85dce4d6228976a890684
Nick White <nawhi3@outlook.com>
2018-09-29 13:51:25 +0100

    Remove missed unnecessary comments

--- src/craftbook/View.java
+++ src/craftbook/View.java
@@ -4,50 +4,22 @@ import java.io.PrintStream;
 import java.time.Duration;
 import java.time.Instant;
 
-/**
- * Base class for text-based views of user data, which
- * are printed to the PrintStream object
- * provided in the constructor when show() is called.
- * @author nick
- */
 public abstract class View {
 
 	protected User targetUser;
 	protected PrintStream outStream;
 	
-	/**
-	 * Create a view which prints to the specified output stream.
-	 * @param user the User whose data is to be viewed
-	 * @param ostream The output stream to print the view to
-	 */
 	public View(User user, PrintStream ostream) {
 		targetUser = user;
 		outStream = ostream;
 	}
 	
-	/**
-	 * Print the content of the view to the view's output stream.
-	 */
 	public void show() {
 		outStream.println(calculate());
 	}
 	
-	/**
-	 * Calculate the content of the view.
-	 * @return string containing the full content of the view 
-	 */
 	public abstract String calculate();
 	
-	/**
-	 * Produce a description of the difference between two times
-	 * in a way appropriate for display to the user. Will display
-	 * the difference in the highest-grained nonzero time unit only.
-	 * @param a the first Instant to compare
-	 * @param b the second Instant to compare 
-	 * @return string describing the time interval between 
-	 * 		   start and end. For example: "moments", "3 seconds",
-	 * 		   "5 minutes", "7 days"
-	 */
 	public static String calcTimespan(Instant a, Instant b) {
 		Duration diff = Duration.between(a,b).abs();
 		
